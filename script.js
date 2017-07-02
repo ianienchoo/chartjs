fe6e1e17f8f53bc478d63a8543c19354257339c5
Ian Choo <ianchoo@Ians-MacBook-Pro.local>
2017-07-02 14:03:19 +0800

    bar and line charts

---
+++ script.js
@@ -0,0 +1,78 @@
+let ctx1 = $("#chart1");
+
+let myChart1 = new Chart(ctx1, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+let ctx2 = $("#chart2");
+
+let myChart2 = new Chart(ctx2, {
+	type: 'line',
+	data: {
+		labels: ["13:00", "13:10", "13:15", "13:20", "13:25"],
+		datasets: [{
+			data: [2,4,6,5,10],
+			backgroundColor: 'rgba(0, 0, 0, 0)',
+			borderColor: 'rgba(54, 162, 235, 1)',
+			borderWidth: 1
+		}]
+	},
+	options: {
+		showLines: true,
+		elements: {
+			line: {
+				tension: 0
+			}
+		}
+	}
+})
+
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
